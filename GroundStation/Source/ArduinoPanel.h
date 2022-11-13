fe0cb7d8104dd0baf107c080f445d0ca46c09ef5
Ilkin Mammadli <81753493+IlkinDigital@users.noreply.github.com>
2022-11-13 14:41:31 -0500

    Added arduino sketch + Serial port input and by-line display in log

--- GroundStation/Source/ArduinoPanel.h
+++ GroundStation/Source/ArduinoPanel.h
@@ -29,8 +29,10 @@ private:
 		frameSize.y -= btnSize.y;
 
 		ImGui::BeginChildFrame(GetID(), frameSize);
-		for (const std::string& log : m_LogStream)
-			ImGui::Text(log.c_str());
+		for (int i = 0; i < m_LogStream.size(); i++) 
+		{
+			ImGui::Text("line %d: %s", i + 1, m_LogStream[i].c_str());
+		}
 		ImGui::EndChildFrame();
 		
 		if (ImGui::Button("On"))
@@ -48,18 +50,25 @@ private:
 
 	virtual void OnUpdate(float timestep) override
 	{
-		m_FrameTime = timestep;
-		m_Timer += timestep;
-
 		if (m_Arduino->IsConnected())
 		{
 			if (m_Arduino->ReadSerialPort(m_Input, MAX_DATA_LENGTH))
 			{
-				std::string input = m_Input;
-				MORPH_INFO(m_Input);
-				m_LogStream.push_back(input);
-				for (int i = 0; i < MAX_DATA_LENGTH; i++)
+				m_MessageBuffer.append(m_Input);
+				for (int i = 0; i < m_MessageBuffer.size(); i++)
+				{
+					if (m_MessageBuffer[i] == '\n')
+					{
+						m_LogStream.push_back(m_MessageBuffer.substr(0, i));
+						m_MessageBuffer = m_MessageBuffer.substr(i + 1);
+						i = 0;
+					}
+				}
+
+				for (int i = 0; i < MAX_DATA_LENGTH; i++) 
+				{
 					m_Input[i] = {};
+				}
 			}
 
 			if (m_Pressed)
@@ -74,14 +83,12 @@ private:
 private:
 	char m_Input[MAX_DATA_LENGTH]{};
 	char m_Output[MAX_DATA_LENGTH]{};
-	const char* m_Port = "\\\\.\\COM4";
+	const char* m_Port = "\\\\.\\COM3";
 
+	std::string m_MessageBuffer;
 	std::vector<std::string> m_LogStream;
 
 	Scope<SerialPort> m_Arduino;
 	std::string m_Command = "ON"; // On or Off
 	bool m_Pressed = true;
-
-	float m_Timer = 0.0f;
-	float m_FrameTime = 0.0f;
 };
---
+++ GroundStation/Source/sketch_nov13a/sketch_nov13a.ino
@@ -0,0 +1,21 @@
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String info;
+    info = Serial.readStringUntil('\n');
+    if (info.equals("ON")) {
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED is ON");
+    } else if (info.equals("OFF")) {
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED is OFF");
+    }
+  }
+}
